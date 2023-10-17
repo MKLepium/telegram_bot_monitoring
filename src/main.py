c3fd86c002230f4012f9fe257741d166164f540b
MKLepium <maximilian.kurzawski@gmail.com>
2023-10-18 00:38:10 +0200

    test

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import telebot
 import dotenv
 import os
+import logging
 
 dotenv.load_dotenv()
 
@@ -9,10 +10,17 @@ if token is None:
     raise Exception('TELEGRAM_TOKEN not found in .env file')
 bot = telebot.TeleBot(token)
 
+# Enable logging
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    print("start command received")
-    print(message.chat.id)
+	logger.info("start command received")
+	logger.debug(message.chat.id)
+	logger.debug(message.chat.type)
+	logger.debug(message.chat)
+
     if message.chat.type == 'private':
         bot.reply_to(message, "Why are you here? This is private")
     else:
@@ -20,12 +28,13 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['stop'])
 def stop(message):
-    print("start command received")
-    print(message.chat.id)
-    print(message.chat.type)
-    print(message.chat)
+	logger.info("stop command received")
+	logger.debug(message.chat.id)
+	logger.debug(message.chat.type)
+	logger.debug(message.chat)
     bot.reply_to(message, "Bye!")
-    bot.stop_polling()
+	exit()
+
 
 # print the group id
 @bot.message_handler(commands=['groupid']) 
